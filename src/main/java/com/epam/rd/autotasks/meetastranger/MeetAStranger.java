799fe4061683803716792b1e54bd7a688c14c1db
Полина Морозова <pmrzv.posya@gmail.com>
2021-07-05 01:46:42 +0300

    add skeletonand tests

---
+++ src/main/java/com/epam/rd/autotasks/meetastranger/MeetAStranger.java
@@ -0,0 +1,6 @@
+package com.epam.rd.autotasks.meetastranger;
+public class MeetAStranger {
+    public static void main(String[] args) {
+        //Write a program, which read a String from System.in and print "Hello, <input string>"
+    }
+}
